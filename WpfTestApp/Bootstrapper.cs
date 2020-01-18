6633b34ecefc08fc2ed22b20a04cfdd980b55a1e
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-01-18 19:15:41 -0300

    Add project files.

---
+++ WpfTestApp/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfTestApp
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
