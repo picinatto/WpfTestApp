f41ef2e14dca7aa6bc25814da5f9b9838b5588c5
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-01-19 22:07:34 -0300

    MVVM

--- WpfTestApp/ViewModels/ShellViewModel.cs
+++ WpfTestApp/ViewModels/ShellViewModel.cs
@@ -7,6 +7,13 @@ namespace WpfTestApp.ViewModels
 {
     public class ShellViewModel : Screen
     {
-        // 
-    }
+		private string _firstName;
+
+		public string FirstName
+		{
+			get { return _firstName; }
+			set { _firstName = value; }
+		}
+
+	}
 }
